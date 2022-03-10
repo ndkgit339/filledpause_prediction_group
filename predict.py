a9cd7d054e4bd82e02d0d0db54f7b973953496f6
Yuta Matsunaga <matsunaga-yuta339@g.ecc.u-tokyo.ac.jp>
2022-03-10 19:59:37 +0900

    [update] prediction

--- predict.py
+++ predict.py
@@ -13,31 +13,32 @@ from dataset import MyDataset
 from module import MyLightningModel
 from util.train_util import collate_fn
 
-def predict(
-    config, trainer, model, out_dir, fp_list):
+def predict(utt_list_path, in_feat_dir, out_feat_dir, out_dir,
+            batch_size, num_workers, trainer, model, fp_list):
 
     # Load utt list
-    with open(to_absolute_path(config.data.utt_list), "r") as f:
-        sentence_list = [l.strip() for l in f]
+    with open(utt_list_path, "r") as f:
+        sentence_list = [l.strip() for l in f if len(l.strip()) > 0]
         utt_name_list = [sen.split(":")[0] for sen in sentence_list]
 
     # Dataset
-    in_feat_dir = Path(to_absolute_path(config.data.in_feat_dir))
-    out_feat_dir = Path(to_absolute_path(config.data.out_feat_dir))
+    in_feat_dir = Path(in_feat_dir)
+    out_feat_dir = Path(out_feat_dir)
 
-    in_feats_paths = [p for p in in_feat_dir.glob("*-feats.npy") if p.stem.split("-feats")[0] in utt_name_list]
-    out_feats_paths = [out_feat_dir / in_path.name for in_path in in_feats_paths]
+    in_feats_paths = [
+        p for p in in_feat_dir.glob("*-feats.npy") 
+        if p.stem.split("-feats")[0] in utt_name_list]
+    out_feats_paths = [
+        out_feat_dir / in_path.name for in_path in in_feats_paths]
 
     dataset = MyDataset(in_feats_paths, out_feats_paths)
-    data_loader = DataLoader(
-        dataset,
-        batch_size=config.data.batch_size,
-        collate_fn=collate_fn,
-        pin_memory=True,
-        num_workers=config.data.num_workers,
-        shuffle=False,
-    )
-    
+    data_loader = DataLoader(dataset,
+                             batch_size=batch_size,
+                             collate_fn=collate_fn,
+                             pin_memory=True,
+                             num_workers=num_workers,
+                             shuffle=False)
+
     # Prediction
     out_utt_list = []
     outputs = trainer.predict(model, data_loader)
@@ -46,33 +47,47 @@ def predict(
         predictions = output["predictions"]
 
         for in_feats_path, prediction in zip(
-            in_feats_paths[batch_idx*config.data.batch_size : (batch_idx+1)*config.data.batch_size],
-            predictions,
-        ):
+                in_feats_paths[
+                    batch_idx * batch_size : (batch_idx + 1) * batch_size],
+                predictions):
+
             utt_id = in_feats_path.stem.split("-")[0]
-            out_utt_list.append(f"{utt_id}:")
             for sen in sentence_list:
-                if sen.startswith(f"{utt_id}:"):
+                if sen.startswith(f"{utt_id}"):
                     utt_text = sen.split(":")[-1]
-            out_utt_list.append(f"\ttarget text: \t{utt_text}")
 
             utt_wo_fps = [
                 w for w in utt_text.split(" ") 
                 if not w.startswith("(F")]
             predicted_fp_tags = torch.argmax(prediction, dim=1)
 
+            # print(prediction.shape)
+            # print(utt_wo_fps)
+            # print(len(utt_wo_fps), len(predicted_fp_tags))
+
             predicted_text = []
             if int(predicted_fp_tags[0]) > 0:
                 predicted_text.append(
-                    fp_list[int(predicted_fp_tags[0]) - 1])
+                    "(F{})".format(
+                        fp_list[int(predicted_fp_tags[0]) - 1]))
             for i in range(len(utt_wo_fps)):
                 predicted_text.append(utt_wo_fps[i])
                 if int(predicted_fp_tags[i + 1]) > 0:
                     predicted_text.append(
-                        "(F{})".format(fp_list[int(predicted_fp_tags[i + 1]) - 1]))
+                        "(F{})".format(
+                            fp_list[int(predicted_fp_tags[i + 1]) - 1]))
 
-            out_utt_list.append(
-                "\tpredicted text: \t{}".format(" ".join(predicted_text)))
+            outtexts = [
+                "{}:".format(utt_id), 
+                "\ttarget text: \t{}".format(utt_text),
+                "\tpredicted text: \t{}".format(" ".join(predicted_text)),
+            ]
+
+            out_utt_list.append(outtexts)
+
+    out_utt_list = [
+        "\n".join(outtexts) 
+        for outtexts in sorted(out_utt_list, key=lambda x: x[0])]
 
     with open(out_dir / "fp_prediction.txt", "w") as f:
         print("writing prediction...")
@@ -83,6 +98,10 @@ def main(config: DictConfig):
 
     # Phase
     phase = "pred"
+
+    # Input directory
+    in_feat_dir = Path(config.data.data_dir) / "infeats"
+    out_feat_dir = Path(config.data.data_dir) / "outfeats"
 
     # Out directory
     exp_dir = Path(to_absolute_path(config[phase].exp_dir))
@@ -111,23 +130,22 @@ def main(config: DictConfig):
         "*-step={}.ckpt".format(str(config[phase].checkpoint.step))
     ))[0]
     pl_model = MyLightningModel.load_from_checkpoint(
-        ckpt_path,
+        str(ckpt_path),
         model=model,
         fp_list=fp_list,
-        strict=False,
-    )
+        strict=False)
 
     # Trainer
     trainer = pl.Trainer(
-        # gpu
         gpus=config[phase].gpus,
         auto_select_gpus=config[phase].auto_select_gpus,
-        default_root_dir=exp_dir,
-        # profiler="simple",
-    )
+        default_root_dir=exp_dir)
 
     # Predict
-    predict(config, trainer, pl_model, out_dir, fp_list)
+    predict(config.data.utt_list, 
+            in_feat_dir, out_feat_dir, out_dir,
+            config.data.batch_size, config.data.num_workers,
+            trainer, pl_model, fp_list)
 
 if __name__=="__main__":
     main()
