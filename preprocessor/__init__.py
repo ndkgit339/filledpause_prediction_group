b605f5289281cd033aaa6b57593cff5c0d00aeb6
Yuta Matsunaga <matsunaga-yuta339@g.ecc.u-tokyo.ac.jp>
2022-03-10 19:59:54 +0900

    [update] preprocess for test

--- preprocessor/__init__.py
+++ preprocessor/__init__.py
@@ -1,5 +1,6 @@
 from .preprocess_feat import extract_feats, extract_feats_test
 from .preprocess_tagtext import process_tagtext
+from .preprocess_morph import process_morph
 from .split_data import split_data
 from .analyze_filler import analyze_fp
 # from .preprocess_utt_list import preprocess_utt_list_utokyo_naist_lecture
